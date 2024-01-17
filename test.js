21d7b57e8860859ffd2c0c163fbc64c791d51e1a
Louise <louiisebld@gmail.com>
2024-01-17 09:41:37 +0100

    avancement

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ const N3 = require('n3');
 const fs = require('fs');
 const express = require("express")
 const app = express()
+const PORT = 8080
 
 const prefixes = {
     ex: 'http://example.org/'
 };
-
 
 const turtleFile = fs.readFileSync('./minecraft.ttl', 'utf8');
 
@@ -20,81 +20,76 @@ parser.parse(turtleFile,
             console.error(error);
         } else if (quad) {
             if (quad)
-                store.addQuad(quad);
+                quad.id = "test"
+            store.addQuad(quad);
         }
         else {
-            store.match(null, prefixes.ex + 'requires', prefixes.ex + 'plank').forEach((quad) => {
-                console.log('Found :', quad);
-            });
+            console.log("Parsing terminé")
         }
     }
 )
 
-// function readAndParseTurtleFile(filePath) {
-//     return new Promise((resolve, reject) => {
-//         const turtleFile = fs.readFileSync(filePath, 'utf8');
-//         const prefixes = {
-//             ex: 'http://example.org/'
-//         };
-
-//         const parser = new N3.Parser();
-//         parser.parse(turtleFile, (error, quad) => {
-//             if (error) {
-//                 reject(error);
-//             } else if (quad) {
-//                 store.addQuad(quad);
-//             } else {
-//                 resolve(store);
-//             }
-//         });
-//     });
-// }
-
-// function executeQuery(store) {
-//     objects = []
-//     return new Promise((resolve, reject) => {
-//         console.log("resultat : ")
-//         store.match(null, prefixes.ex + 'requires', prefixes.ex + 'plank').forEach((quad) => {
-//             objects.push(quad)
-//         });
-//     });
-// }
-
-// Utilisation des promesses
-// readAndParseTurtleFile('./minecraft.ttl')
-//     .then((store) => {
-//         console.log('Fichier Turtle traité avec succès.');
-//         // Affiche tout le store si nécessaire
-//         // printStore(store);
-
-//         return executeQuery(store);
-//     })
-//     .then((results) => {
-//         console.log('Requête exécutée avec succès.');
-//         console.log('Résultats :', results);
-//     })
-//     .catch((error) => {
-//         console.error('Une erreur s\'est produite :', error);
-//     });
-
-
 app.get("/", (req, res) => {
-    objects = []
-    string = ""
-    res.send('yoyoyo')
-
+    res.send('Bienvenue')
 })
-
+// store.match(null, prefixes.ex + 'requires', prefixes.ex + 'plank').forEach((quad) => {
+//     console.log('Found :', quad);
+// });
 app.get("/objects", (req, res) => {
-    // res.send(objects)
+    quads = []
+    store.forEach((quad) => {
+        quads.push(quad)
+    });
+    res.send(quads)
 })
 
-app.listen(8080, () => {
-    // readAndParseTurtleFile('./minecraft.ttl')
+app.get("/objects/:material", async (req, res) => {
+    try {
+        const quads = await getQuads(req.params.material);
+        console.log("Quads:", quads);
+        res.send(quads);
+    } catch (error) {
+        console.error("Une erreur s'est produite :", error);
+        res.status(500).send("Une erreur s'est produite");
+    }
+});
 
+function getQuads(material) {
+    return new Promise((resolve, reject) => {
+        let quads = [];
+        store.match(null, prefixes.ex + 'requires', prefixes.ex + material).forEach((quad) => {
+            quads.push(quad);
+            console.log("quads", quads);
+        });
 
-    // print le store
-    store.forEach((quad) => {
-        console.log(quad);
+        // Utiliser setImmediate pour rendre cela asynchrone
+        setImmediate(() => {
+            console.log("testtesttest");
+            resolve(quads);
+        });
     });
+}
+
+
+
+// // récupérer un objet fait d'un certain matériau
+// app.get("/objects/:material", (req, res) => {
+//     // res.send(store.match(null, prefixes.ex + 'requires', prefixes.ex + req.params.material))
+//     let quads = []
+//     store.match(null, prefixes.ex + 'requires', prefixes.ex + 'plank').forEach((quad) => {
+//         quads.push(quad);
+//         console.log("quads", quads)
+//     });
+//     console.log("testtesttest")
+
+//     // store.match(null, prefixes.ex + 'requires', prefixes.ex + req.params.material).forEach((quad) => {
+//     //     quads.push(quad);
+//     // });
+//     // console.log(quads)
+//     res.send(quads)
+// })
+
+
+app.listen(PORT, () => {
+    console.log("Serveur démarré sur le port PORT")
 })
