d021ab435322fcc14c5479510b923d3317bf78db
Louise <louiisebld@gmail.com>
2024-01-17 10:48:28 +0100

    update route material, crafts

--- test.js
+++ test.js
@@ -9,9 +9,7 @@ const prefixes = {
 };
 
 const turtleFile = fs.readFileSync('./minecraft.ttl', 'utf8');
-
 const store = new N3.Store();
-
 const parser = new N3.Parser();
 
 parser.parse(turtleFile,
@@ -19,8 +17,6 @@ parser.parse(turtleFile,
         if (error) {
             console.error(error);
         } else if (quad) {
-            if (quad)
-                quad.id = "test"
             store.addQuad(quad);
         }
         else {
@@ -29,23 +25,72 @@ parser.parse(turtleFile,
     }
 )
 
+function extractMaterialFromQuad(quads, left_or_right) {
+    const materialNamesSet = new Set();
+
+    quads.forEach(quad => {
+
+        if (left_or_right == "right") {
+            if (quad.subject && quad.subject.value) {
+                materialNamesSet.add(getName(quad.subject.value));
+            }
+        }
+        else {
+            if (quad.object && quad.object.value) {
+                materialNamesSet.add(getName(quad.object.value));
+            }
+        }
+    });
+
+    const materialNamesArray = [...materialNamesSet];
+    return materialNamesArray
+}
+
+function getName(value) {
+    return value.replace(prefixes.ex, "");
+}
+
+function getCraft(material) {
+    return new Promise((resolve, reject) => {
+        let quads = [];
+        store.match(prefixes.ex + material, prefixes.ex + 'canCraft', null).forEach((quad) => {
+            quads.push(quad);
+            console.log("quads", quads);
+        });
+
+        // Utiliser setImmediate pour rendre cela asynchrone
+        setImmediate(() => {
+            resolve(quads);
+        });
+    });
+}
+
+function getRequireMaterialsForACraft(craft) {
+    return new Promise((resolve, reject) => {
+        let quads = [];
+        store.match(null, prefixes.ex + 'canCraft', prefixes.ex + craft).forEach((quad) => {
+            quads.push(quad);
+            console.log("quads", quads);
+        });
+
+        // Utiliser setImmediate pour rendre cela asynchrone
+        setImmediate(() => {
+            resolve(quads);
+        });
+    });
+}
+
+// --------------------------------------------------------------- Routes
+
 app.get("/", (req, res) => {
     res.send('Bienvenue')
 })
-// store.match(null, prefixes.ex + 'requires', prefixes.ex + 'plank').forEach((quad) => {
-//     console.log('Found :', quad);
-// });
-app.get("/objects", (req, res) => {
-    quads = []
-    store.forEach((quad) => {
-        quads.push(quad)
-    });
-    res.send(quads)
-})
 
-app.get("/objects/:material", async (req, res) => {
+app.get("/objects/crafts/:material", async (req, res) => {
+    // les crafts qui nécessitent le matériau
     try {
-        const quads = await getQuads(req.params.material);
+        let quads = await getCraft(req.params.material);
+        quads = extractMaterialFromQuad(quads);
         console.log("Quads:", quads);
         res.send(quads);
     } catch (error) {
@@ -54,41 +99,18 @@ app.get("/objects/:material", async (req, res) => {
     }
 });
 
-function getQuads(material) {
-    return new Promise((resolve, reject) => {
-        let quads = [];
-        store.match(null, prefixes.ex + 'requires', prefixes.ex + material).forEach((quad) => {
-            quads.push(quad);
-            console.log("quads", quads);
-        });
-
-        // Utiliser setImmediate pour rendre cela asynchrone
-        setImmediate(() => {
-            console.log("testtesttest");
-            resolve(quads);
-        });
-    });
-}
-
-
-
-// // récupérer un objet fait d'un certain matériau
-// app.get("/objects/:material", (req, res) => {
-//     // res.send(store.match(null, prefixes.ex + 'requires', prefixes.ex + req.params.material))
-//     let quads = []
-//     store.match(null, prefixes.ex + 'requires', prefixes.ex + 'plank').forEach((quad) => {
-//         quads.push(quad);
-//         console.log("quads", quads)
-//     });
-//     console.log("testtesttest")
-
-//     // store.match(null, prefixes.ex + 'requires', prefixes.ex + req.params.material).forEach((quad) => {
-//     //     quads.push(quad);
-//     // });
-//     // console.log(quads)
-//     res.send(quads)
-// })
-
+// les matériaux nécessaires pour un craft
+app.get("/objects/materials/:material", async (req, res) => {
+    try {
+        let quads = await getRequireMaterialsForACraft(req.params.material);
+        quads = extractMaterialFromQuad(quads, "right");
+        console.log("Quads:", quads);
+        res.send(quads);
+    } catch (error) {
+        console.error("Une erreur s'est produite :", error);
+        res.status(500).send("Une erreur s'est produite");
+    }
+});
 
 app.listen(PORT, () => {
     console.log("Serveur démarré sur le port PORT")
