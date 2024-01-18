5f48f271fa562f42095d33599eb110eb4bf542c5
Louise <louiisebld@gmail.com>
2024-01-18 20:53:40 +0100

    index and item

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const app = express()
 
 const index = []
 
-
 app.use("/css", express.static(__dirname + "/css"))
 app.use("/js", express.static(__dirname + "/js"))
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html")
+	res.sendFile(__dirname + "/html/index.html")
+})
+
+app.get("/item", (req, res) => {
+	res.sendFile(__dirname + "/html/item.html")
 })
 
 app.listen(8080)
